6d8b6bc2e37b63f397fcf258f495707c9be27d5d
InaderaRantarou <94066215+InaderaRantarou@users.noreply.github.com>
2024-06-05 18:37:42 +0700

    FrequentElement.c

--- FrequentElement.c
+++ FrequentElement.c
@@ -5,17 +5,17 @@ void findMostFrequentElement(int arr[], int size)
     int maxCount = 0;
     int mostFrequent = arr[0];
     for (int i = 0; i < size; ++i) 
-	  {
+    {
         int count = 0;
         for (int j = 0; j < size; ++j) 
-  		  {
+  	{
             if (arr[j] == arr[i]) 
-  			    {
+  	    {
                 count++;
             }  
         }
         if (count > maxCount) 
-  		  {
+  	{
             maxCount = count;
             mostFrequent = arr[i];
         }
