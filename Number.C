775c9bb0a558db22edb05b47d639e5d49be7511d
InaderaRantarou <94066215+InaderaRantarou@users.noreply.github.com>
2024-06-05 10:28:21 +0700

    Number.C

--- Number.C
+++ Number.C
@@ -11,26 +11,26 @@ int main()
     printf("Enter number 2nd: ");
     scanf("%d", &end);
     if (start >= end) 
-	{
+    {
         printf("number 1st smaller than 2nd number.\n");
         return 1;
     }
     for (int i = start; i <= end; ++i) 
-	{
+    {
         if (i % 3 == 0 && i % 5 == 0) 
-		{
+	{
             printf("A ");
         } 
-		else if (i % 3 == 0) 
-		{
+	else if (i % 3 == 0) 
+	{
             printf("F ");
         } 
-		else if (i % 5 == 0) 
-		{
+	else if (i % 5 == 0) 
+	{
             printf("Z ");
         } 
-		else 
-		{
+	else 
+	{
             printf("%d ", i);
         }
     }
