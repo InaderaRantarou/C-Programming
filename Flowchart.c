307e4cec6dae6b03ae7718a577e24b814dd0d273
InaderaRantarou <94066215+InaderaRantarou@users.noreply.github.com>
2024-06-05 11:36:21 +0700

    Flowchart.c

--- Flowchart.c
+++ Flowchart.c
@@ -2,26 +2,26 @@
 #include <stdio.h>
 int isPrime(int n)
 {
-	if (n < 2)
-		return 0;
+   if (n < 2)
+	return 0;
 
-	for (int i = 2; i * i <= n; ++i)
-	{
-		if(n % i == 0)
-			return 0;
-	}
-	return 1;
+   for (int i = 2; i * i <= n; ++i)
+   {
+   if(n % i == 0)
+	return 0;
+   }
+   return 1;
 }
 
 int main()
 {
-	int n;
-	printf("Input n: ");
-	scanf("%d", &n);
-	for (int i = 1; i <= n; ++i)
-	{
-		if (n % i == 0 && isPrime(i))
-			printf("%d ", i);
-	}
-	return 0;
+   int n;
+   printf("Input n: ");
+   scanf("%d", &n);
+   for (int i = 1; i <= n; ++i)
+   {
+   if (n % i == 0 && isPrime(i))
+      printf("%d ", i);
+   }
+   return 0;
 }
