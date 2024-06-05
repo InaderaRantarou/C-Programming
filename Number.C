eb38fcceda1cf53a3dc7a787e8ed2104b6c0b7ae
InaderaRantarou <94066215+InaderaRantarou@users.noreply.github.com>
2024-06-05 10:20:51 +0700

    Number.C

--- Number.C
+++ Number.C
@@ -1,3 +1,4 @@
+// Write a program to input 2 integers (the first number is smaller than the second number). Print the numbers from the first number to the second number, but replace numbers divisible by 3 with 'F', divisible by 5 with 'Z', and divisible by both 3 and 5 with 'A'.
 #include <stdio.h>
 int main() 
 {
