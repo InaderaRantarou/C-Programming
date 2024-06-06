c3dd9f8aa30318182c257a63039c74bdca872160
InaderaRantarou <94066215+InaderaRantarou@users.noreply.github.com>
2024-06-06 18:32:48 +0700

    Test5.c

--- FrequentElement.cpp
+++ FrequentElement.cpp
@@ -1,42 +1,35 @@
-#include <iostream>
-using namespace std;
-
-void findMostFrequentElement(int arr[], int size) {
-    int maxCount = 0;
-    int mostFrequent = arr[0];
-    
-    for (int i = 0; i < size; ++i) {
-        int count = 0;
-        for (int j = 0; j < size; ++j) {
-            if (arr[j] == arr[i]) {
-                count++;
-            }
-        }
-        
-        if (count > maxCount) {
-            maxCount = count;
-            mostFrequent = arr[i];
-        }
-    }
-    
-    cout << "The most frequent element is: " << mostFrequent << endl;
-}
-
-int main() {
+// Write a program to input an array (array size and array elements). Print the most frequent element in the array of integers.
+#include<stdio.h>
+int main() 
+{
     int size;
-    cout << "Enter the size of the array: ";
-    cin >> size;
-
-    int* arr = new int[size];  // Dynamic allocation of the array
-
-    cout << "Enter " << size << " integers: ";
-    for (int i = 0; i < size; ++i) {
-        cin >> arr[i];
+    int maxfreq = 0, position =0; //luu vi tri cua so trong mang
+    
+    printf("Nhap vao kich thuoc mang (size of array): ");
+    scanf("%d", &size);
+    
+    int arr[size];  
+  	for(int i = 0 ; i < size; i++)
+    {  
+        printf("Nhap vao gia tri [%d] :  ", i);
+        scanf("%d", &arr[i]);
     }
-
-    findMostFrequentElement(arr, size);
-
-    delete[] arr;  // Free dynamically allocated memory
-
-    return 0;
+    for(int i = 0; i < size; i++)
+    {	//dem tan so xuat hien cua 1 so
+        int freq = 1;
+        for(int j = i + 1; j < size; j++)
+        {
+            if(arr[i] == arr[j])
+            {
+                freq = freq + 1;
+            }
+        }      
+        if(freq > maxfreq)
+        {
+            maxfreq = freq;
+			position = i; //luu vi tri cua so maxfreq_number trong mang (so thu i)
+        }
+    }
+    printf("so xuat hien nhieu nhat la: %d", arr[position]);
+    printf("\nSo lan xuat hien: %d", maxfreq); // De bai khong hoi
 }
